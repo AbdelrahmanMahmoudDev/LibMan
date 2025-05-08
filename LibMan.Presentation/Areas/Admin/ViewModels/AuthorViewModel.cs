dc0eff31e2ed206e264ae6bd444ed2725b3779cd
AbdelrahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2025-05-09 01:46:45 +0300

    Improved validation for Author

--- LibMan.Presentation/Areas/Admin/ViewModels/AuthorViewModel.cs
+++ LibMan.Presentation/Areas/Admin/ViewModels/AuthorViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using LibMan.Domains;
+using LibMan.Presentation.CustomValidationAttributes;
 
 namespace LibMan.Presentation.Areas.Admin.ViewModels
 {
@@ -7,11 +8,12 @@ namespace LibMan.Presentation.Areas.Admin.ViewModels
     {
         public const int BioMax = 300;
         public int Id { get; set; }
-        [Required(ErrorMessage = "*")]
+        [Required(ErrorMessage = "* This field is required")]
+        [FullName(ErrorMessage = "Please enter a valid full name")]
         public string FullName { get; set; } = string.Empty;
         [MaxLength(100, ErrorMessage = "The email cannot exceed 100 characters")]
         [EmailAddress(ErrorMessage = "Invalid email format")]
-        [Required(ErrorMessage = "*")]
+        [Required(ErrorMessage = "* This field is required")]
         public string Email { get; set; } = string.Empty;
         [MaxLength(BioMax, ErrorMessage = $"The biography cannot exceed the maximium letter count")]
         public string? Website { get; set; }
---
+++ LibMan.Presentation/CustomValidationAttributes/FullNameAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibMan.Presentation.CustomValidationAttributes
+{
+    public class FullNameAttribute : ValidationAttribute
+    {
+        private ValidationResult? CheckStringIs4Words(string[] words)
+        {
+            if (words.Length != 4)
+            {
+                return new ValidationResult($"You must enter exactly your first 4 names. Current names: {words.Length}");
+            }
+            return null;
+        }
+
+        private ValidationResult? CheckWordIs2CharsAtleast(string[] words)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length < 2)
+                {
+                    return new ValidationResult($"Each name must have atleast 2 characters, Invalid name: {words[i]}");
+                }
+            }
+            return null;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if(value is not string fullName || string.IsNullOrWhiteSpace(fullName))
+            {
+                return new ValidationResult("Full name cannot be empty.");
+            }
+
+            string[] words = fullName.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            ValidationResult? Result = CheckStringIs4Words(words);
+
+            Result = CheckWordIs2CharsAtleast(words);
+
+            return ValidationResult.Success;
+        }
+    }
+}
